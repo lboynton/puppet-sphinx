59ffbb2cd12f43bd0e557f0a674a3bfb6c7832ca
Lee Boynton <lee@lboynton.com>
2012-10-17 11:19:36 +0100

    Specify mysql connection details in sphinx::config params so that the same config can be used on different servers with different connection details.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,10 +1,25 @@
-define sphinx::config($source) {
-    file { "/etc/sphinx.d/${name}/":
+define sphinx::config(
+    $source,
+    $type   => 'mysql',
+    $host   => '127.0.0.1',
+    $user   => undef,
+    $pass   => undef,
+    $db     => undef,
+    $port   => 3306,
+    $site   => undef,
+) {
+    file { "/etc/sphinx.d/${site}/":
         ensure => directory,
         source => $source,
         recurse => true,
         purge   => true,
         force   => true,
+        owner   => 'root',
+        group   => 'root',
+    }
+
+    file { "/etc/sphinx.d/${site}/default.source":
+        content => template("sphinx/default.source.erb"),
         owner   => 'root',
         group   => 'root',
     }
